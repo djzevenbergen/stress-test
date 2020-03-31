bfb80ce78e2908e47d8c430b18230fdf65c75658
djzevenbergen <djzevenbergen@gmail.com>
2020-03-31 13:46:15 -0700

    do small palindrome excercise

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,47 @@ var makeSuggestion = function (warn, health) {
     $("#suggestionOne").removeClass("hide");
   }
 
-
 }
+
+
+function palindrome(word) {
+  var forward = word.split("");
+  var back = word.split("");
+  var backward = back.reverse().slice();
+
+  var f = forward.join();
+  var b = backward.join();
+
+  if (f === b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//   var f_index = 0;
+//   var b_index = backward.length - 1;
+//   var isTrue = true;
+//   while (b_index >= 0) {
+//     if (forward[f_index] === backward[b_index]) {
+//       b_index--;
+//       f_index++;
+//     }
+//     else {
+//       isTrue = false;
+//     }
+//   }
+//   return isTrue;
+// }
 
 
 $(document).ready(function () {
   $("#nextQuestion").click(function () {
     $("#questionOne").hide();
     $("#questionTwo").show();
+    alert(palindrome("Hello olleH"));
+
+
   });
 
   $("#stressForm").submit(function (event) {
@@ -30,8 +63,6 @@ $(document).ready(function () {
       var warningSigns = $(this).val();
       warningArrays.push(warningSigns);
 
-
-
     });
 
     $("input:checkbox[name=health-symptom]:checked").each(function () {
@@ -43,12 +74,7 @@ $(document).ready(function () {
 
     makeSuggestion(warningArrays, healthArrays);
 
-
-
-
-
   });
 
+});
 
-
-})
