d10a43ed92a564887ccc36ea81d69586aafcfbce
djzevenbergen <djzevenbergen@gmail.com>
2020-03-31 13:17:00 -0700

    finish algorithm and complete project

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 var makeSuggestion = function (warn, health) {
-
+  if (health.includes("death")) {
+    $("#suggestionThree").removeClass("hide");
+  } else if (warn.includes("doom")) {
+    $("#suggestionTwo").removeClass("hide");
+  } else {
+    $("#suggestionOne").removeClass("hide");
+  }
 
 
 }
@@ -16,19 +22,26 @@ $(document).ready(function () {
 
     $("#stressForm").hide();
 
+    var warningArrays = [];
+    var healthArrays = [];
+
     $("input:checkbox[name=warning-sign]:checked").each(function () {
 
       var warningSigns = $(this).val();
+      warningArrays.push(warningSigns);
 
 
-    })
+
+    });
 
     $("input:checkbox[name=health-symptom]:checked").each(function () {
 
       var healthSymptoms = $(this).val();
+      healthArrays.push(healthSymptoms);
 
-    })
+    });
 
+    makeSuggestion(warningArrays, healthArrays);
 
 
 
